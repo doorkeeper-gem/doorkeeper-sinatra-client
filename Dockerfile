6c561739f3f0ac982ad9459ca856602fac598ee8
Felipe Philipp <felipeelias@gmail.com>
2019-04-14 15:35:08 +0200

    Dockerize application

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ruby:2.6
+
+LABEL maintainer="felipe@yerba.dev"
+
+RUN gem install bundler:2.0.1
+
+WORKDIR /usr/src/app
+
+COPY Gemfile Gemfile.lock ./
+
+RUN bundle install --jobs 4
+
+COPY . .
+
+CMD ["bundle", "exec", "puma", "-C", "config/puma.rb"]
---
+++ config/puma.rb
@@ -0,0 +1,7 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['SINATRA_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 9292
+environment ENV['RACK_ENV'] || 'development'
